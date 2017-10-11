819cab2613ea75a400267ea7c01791e7513d27d8
SkyLuv <SkyLuv@SKYLUV-PC>
2017-10-11 12:03:43 -0700

    Working as expected at this point. no organization

--- SkyPatcher/Start.cpp
+++ SkyPatcher/Start.cpp
@@ -5,53 +5,80 @@
 #include <chrono>
 #include "Windows.h"
 
+uint32_t dead = 0;
+
+INPUT click;
+
+
 Memory memory;
 
 namespace Rez
 {
-	typedef void(__thiscall * Func)(void* pThis, void* first, int32_t flag, void* third);
+//	typedef void(__thiscall * Func)(void* pThis, void* first, int32_t flag, void* third);
+//	Func Func_ = (Func)addr_func;
 
+	uint32_t firstarg = 0;
+	uint32_t secondarg = 5;
+	uint32_t *  thirdarg = nullptr;
+	uint32_t    returnaddr = 0;
+	void * pThis = *(void**)0x0344EB08;
 	uint32_t addr_func = 0x011BE4D0;
-	Func Func_ = (Func)addr_func;
+
+
 	uint32_t off_funcgrabargs = 0x10DEB0F;
 	uint32_t off_firstarg = 0x2748e78;
-	void * pThis = *(void**)0x0344EB08;
-	uint32_t *  ReviveBox_thirdarg;
-	uint32_t    ReviveBox_returnaddr;
-	void  ReviveBox_GrabArg();
+	
+	void  GrabArg();
 
 };
 
-
-
 namespace LocalChara
 {
+	uint32_t off_health = 0x134;
 	uint32_t addr_base = 0;
-	uint32_t off_health = 0x140;
-	uint32_t off_func = 0x645B80;
+	uint32_t off_func = 0x6D49D0;
 	uint32_t returnaddr = 0;
 	void GrabBase();
 }
 
-void __declspec(naked) Rez::ReviveBox_GrabArg()
+void __declspec(naked) Rez::GrabArg()
 {
 
 	__asm
 	{
+	
+		cmp dead, 0
+		je original
+		
+		push edx
 		push ecx
+		push edi
+		push esi
 		push eax
 
-		//get the third argument for the box function from here
-		mov eax, [ebp - 0x34]
-		mov ReviveBox_thirdarg, eax
+		mov ecx, [pThis]
+		//get the third arg
+		mov eax, dword ptr[ebp - 0x34]
+		push eax
+		push secondarg
+
+		push firstarg
+
+		mov dead, 0
+		call [addr_func]
 
 		pop eax
+		pop esi
+		pop edi
 		pop ecx
+		pop edx
 
+		original:
+		
 		call edx
 		sub eax, 0x00
 
-		jmp[ReviveBox_returnaddr]
+		jmp[returnaddr]
 	}
 
 }
@@ -67,7 +94,6 @@ void __declspec(naked) LocalChara::GrabBase()
 	}
 }
 
-
 void Start::Start()
 {
 
@@ -78,17 +104,47 @@ void Start::Start()
 	Setup_ReviveBoxInf(base);
 	Setup_LocalCharaInf(base);
 
-	
 	while (true)
 	{
-	
-		if (*(float*)(LocalChara::addr_base + LocalChara::off_health) == 0)
-		{
-			Rez::Func_(Rez::pThis, (void*)(base + Rez::off_firstarg), 5, (void*)Rez::ReviveBox_thirdarg);
-		}
-		else 
 
-		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		if (LocalChara::addr_base != 0)
+		{
+			std::cout << "address of health: " << LocalChara::addr_base + LocalChara::off_health << std::endl;
+			while (true)
+			{
+				if (*(float*)(LocalChara::addr_base + LocalChara::off_health) == 0)
+				{
+					//	Rez::Func_(Rez::pThis, (void*)(base + Rez::off_firstarg), 5, (void*)Rez::thirdarg);
+					dead = 1;
+					//*(float*)(LocalChara::addr_base + LocalChara::off_health) = 1;
+					while (*(float*)(LocalChara::addr_base + LocalChara::off_health) == 0)
+					{
+						click.type = INPUT_MOUSE;
+						click.mi.dwFlags = (MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_LEFTDOWN);
+						
+						SendInput(1, &click, sizeof(INPUT));
+
+						click.mi.dwFlags = (MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_LEFTUP);
+
+						SendInput(1, &click, sizeof(INPUT));
+					}
+
+				}
+				else if (GetAsyncKeyState(VK_UP))
+				{
+					dead = 1;
+				}
+				else
+				{
+					std::this_thread::sleep_for(std::chrono::milliseconds(100));
+				}
+			}
+		
+
+		}
+		std::cout << &LocalChara::addr_base << std::endl;
+		std::this_thread::sleep_for(std::chrono::milliseconds(500));
+		
 	}
 	
 		
@@ -98,15 +154,17 @@ void Start::Setup_ReviveBoxInf(const uint32_t & ModuleBase)
 {
 	auto grabargs_functionaddr = (ModuleBase + Rez::off_funcgrabargs);
 
-	Rez::ReviveBox_returnaddr = (grabargs_functionaddr + 0x05);
+	Rez::firstarg = (ModuleBase + Rez::off_firstarg);
 
-	memory.WriteJump((char*)grabargs_functionaddr, (uint32_t)Rez::ReviveBox_GrabArg);
+	Rez::returnaddr = (grabargs_functionaddr + 0x05);
+
+	memory.WriteJump((char*)grabargs_functionaddr, (uint32_t)Rez::GrabArg);
 
 }
 
 void Start::Setup_LocalCharaInf(const uint32_t & ModuleBase)
 {
-	auto funcaddress = (ModuleBase + 0x645B80);
+	auto funcaddress = (ModuleBase + LocalChara::off_func);
 
 	LocalChara::returnaddr = (funcaddress + 0x07);
 
